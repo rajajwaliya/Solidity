c7604fcb4a4a3562956871837487172968edb319
rajajwaliya <raj.ajwaliya@jbspl.com>
2019-01-08 18:07:45 +0530

    add Insurance Contract

---
+++ Insurance Contract/insurance.sol
@@ -0,0 +1,219 @@
+pragma solidity ^0.5.1;
+pragma experimental ABIEncoderV2;
+
+contract Seller{
+    address payable owner;
+    uint[] products;
+    
+
+    struct ProductsDetails{
+        
+        address payable owner;
+        address  productaddress;
+        uint id;
+        uint price;
+        string desc;
+        bool available;
+    }
+    
+    
+    mapping(address => bool) IsVerifiedSeller;
+    
+    
+    mapping (uint => ProductsDetails) proOption;
+  //  mapping (address => mapping(address => uint)) public Deposite;
+    mapping (address => mapping(address => mapping(address => uint))) SellerDeposite;
+    
+     constructor() public payable{
+        //set owner      
+        owner=msg.sender;
+     }
+     
+     function AllowSeller(address _seller) public returns(bool successful){
+         require(owner==msg.sender);
+         
+        IsVerifiedSeller[_seller]=true;
+         return true;
+     }
+     
+     function setProduct(address payable _seller,address _prdadd, uint _productid,uint _price,string memory _des)public returns(bool){
+        
+        require(IsVerifiedSeller[_seller]==true);
+        require(_seller==msg.sender);
+        
+        if(_productid==proOption[_productid].id){revert();}
+        
+        ProductsDetails memory prduct = ProductsDetails( _seller,_prdadd,_productid,_price,_des,true);
+        proOption[_productid]=prduct;
+        products.push(_productid);
+        return true;
+    } 
+    
+     function getProduct(uint _productid) public view returns(address _owner,address product,uint price,string memory _des,bool _available){
+
+        return (proOption[_productid].owner,proOption[_productid].productaddress,proOption[_productid].price,proOption[_productid].desc,proOption[_productid].available);
+        
+    }
+    
+    
+     function balanceOf(address _address)public view returns(uint256 balance) {
+
+        return (_address.balance)/(10**18);
+
+    }
+    
+    function sellerDeposite(address _seller,address _Insurance,address _Buyer)public view returns (uint _balance){
+        return (SellerDeposite[_seller][_Insurance][_Buyer])/(10**18);
+    }
+    
+    function getallProductIds() public view returns (uint[] memory){
+        return products;
+    }
+    
+    
+}
+
+
+
+
+contract Insurance is Seller{
+    
+        address owner;
+        address[] InsuranceAddresses;
+    
+        mapping(address => bool) public IsVerified;
+        mapping(address => uint) intestrate;
+        mapping(address => IMIaddress) ownAddress;
+        mapping(address => mapping(uint => address)) FindInsurance;
+        mapping(address => mapping( uint => mapping(address => uint))) InsuranceAndBuyer;
+        mapping (address => mapping(address =>mapping(uint => uint))) Remining;
+        mapping(address => mapping( address => mapping(uint => bool))) temp;
+        mapping(address => mapping(address => mapping(uint => uint))) starttime;
+        
+        struct IMIaddress{
+            address payable InsuranceAddress;
+        }
+
+    function getInterestRate(address _insuraceAdd) public view returns(uint _intestrate){
+      return intestrate[_insuraceAdd];
+      
+    }
+    
+    function AllowInsuranceAddress(address payable _insuraceAdd,uint _intestrate) public returns(bool successful){
+        
+        require(msg.sender==owner); 
+        require(IsVerified[_insuraceAdd]==false);
+        require(_intestrate>0);
+        
+        IsVerified[_insuraceAdd]=true;
+        IMIaddress memory imi = IMIaddress(_insuraceAdd);
+        ownAddress[_insuraceAdd]=imi;
+        intestrate[_insuraceAdd]=_intestrate;
+        InsuranceAddresses.push(_insuraceAdd);
+        return true;
+        
+    }
+    
+
+    function ProductPriceAfterEmi(uint _productId, address _insuraceAdd) public view returns (uint _price){
+    
+       require(IsVerified[_insuraceAdd]==true,"Insurance address not Verified");
+     
+      return (Seller.proOption[_productId].price+(((Seller.proOption[_productId].price)*(intestrate[_insuraceAdd]))/100));
+    
+    }
+
+    function getEmiPrice (uint _productId, address _insuraceAdd)public view returns(uint _EMIprice){
+        
+    
+         return ((ProductPriceAfterEmi(_productId,_insuraceAdd))/12);
+    
+    }
+    
+      function transfer(address ToSellerAddress,address payable Buyer,uint _productid) payable public{
+          
+           require(IsVerified[msg.sender]==true,"Insurance address not Verified");
+           require(Seller.IsVerifiedSeller[ToSellerAddress]==true,"Invalid Seller");
+           require(msg.value>=Seller.proOption[_productid].price,"price error");
+           require(ToSellerAddress==Seller.proOption[_productid].owner,"owner can not buy it's own product");
+           require(Seller.proOption[_productid].available==true,"item not available");
+           require(temp[msg.sender][Buyer][_productid]==true,"product error");
+        //   require(InsuranceAndBuyer[msg.sender][_productid][Buyer]>=getEmiPrice(_productid,msg.sender),"error 6");
+          
+        
+        
+        Seller.proOption[_productid].owner.transfer(msg.value);
+        // Deposite[ToSellerAddress][FromInsuranceAddress] = Deposite[ToSellerAddress][FromInsuranceAddress] + msg.value;
+        SellerDeposite[ToSellerAddress][msg.sender][Buyer] = SellerDeposite[ToSellerAddress][msg.sender][Buyer] + msg.value;
+        Seller.proOption[_productid].owner=Buyer;
+        Seller.proOption[_productid].available=false;
+        FindInsurance[Buyer][_productid]=msg.sender;
+        uint userprice = ProductPriceAfterEmi(_productid,msg.sender);
+        starttime[msg.sender][Buyer][_productid]=now;
+        Remining[Buyer][msg.sender][_productid] = ((userprice)*(10**18));
+     
+
+    }
+    
+    function getallInsuranceAddress() public view returns (address[] memory){
+        
+        return InsuranceAddresses;
+        
+    }
+    
+    function CheckforBuyer(address Buyer,uint _productId) public view returns (bool Success){
+        return temp[msg.sender][Buyer][_productId];
+        
+    }
+    
+    
+}
+
+contract Buyer is Insurance{
+    
+    
+    
+     
+     
+    function BuyProduct(uint _productId, address _InsuranceAddress) public returns (bool Successess){
+        
+        uint userprice = ProductPriceAfterEmi(_productId,_InsuranceAddress);
+        
+        require((msg.sender).balance>=((userprice)*(10**18)),"4 error");
+        require(IsVerified[_InsuranceAddress]==true,"3 error");
+        require(Seller.proOption[_productId].available==true,"2 error");
+        require(temp[_InsuranceAddress][msg.sender][_productId]==false);
+        require(msg.sender!=Seller.proOption[_productId].owner,"owner can not buy it's own product");
+        
+       temp[_InsuranceAddress][msg.sender][_productId]=true;
+        return true;
+        
+    }
+    
+    function PayEMI(uint _productId, address _InsuranceAddress)public payable returns (bool Successess){
+      
+       require(now >= starttime[_InsuranceAddress][msg.sender][_productId] + 1 minutes);
+       require(IsVerified[_InsuranceAddress]==true,"3 error");
+       require(proOption[_productId].owner==msg.sender);
+       require(temp[_InsuranceAddress][msg.sender][_productId]==true);
+       require(_InsuranceAddress==FindInsurance[msg.sender][_productId],"wrong InsuranceAddress");
+        
+        uint EMIprice = getEmiPrice(_productId,_InsuranceAddress);
+        
+        require(msg.value>=EMIprice);
+        
+        ownAddress[_InsuranceAddress].InsuranceAddress.transfer(msg.value);
+       // Seller.Deposite[_InsuranceAddress][msg.sender] = Seller.Deposite[_InsuranceAddress][msg.sender] + msg.value;
+        InsuranceAndBuyer[_InsuranceAddress][_productId][msg.sender] = InsuranceAndBuyer[_InsuranceAddress][_productId][msg.sender] + msg.value;
+        Remining[msg.sender][_InsuranceAddress][_productId] = Remining[msg.sender][_InsuranceAddress][_productId] - (msg.value);
+        starttime[_InsuranceAddress][msg.sender][_productId]=now;
+        return true;
+      
+        }
+  
+      //Remining method can return Remining ether need to payed by Buyer
+      function ReminingEMI(address _Buyer,address payable _InsuranceAddress,uint _productId)public view returns (uint){
+          return ((Remining[_Buyer][_InsuranceAddress][_productId])/(10**18));
+      }
+    
+}
